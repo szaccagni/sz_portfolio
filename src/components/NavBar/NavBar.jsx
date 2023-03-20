ead4c02df9690c5cf0d97d76526c9e6f7fc07c72
szaccagni <sarahzaccagni@gmail.com>
2023-03-19 21:11:03 -0400

    init commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <>
+            <h1>Sarah Zaccagni</h1>
+            <a>home</a>
+            <a>projects</a>
+            <a>info</a>
+            <a>contact</a>
+        </>
+    )
+}
---
+++ src/pages/App/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+import NavBar from '../../components/NavBar/NavBar'
+
+export default function App() {
+  return (
+    <main className="App">
+      <NavBar />
+    </main>
+  )
+}
