7e7d324000ea7a42ed814e4b99235a533f5478a2
szaccagni <sarahzaccagni@gmail.com>
2023-03-19 21:26:55 -0400

    adds routes

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,20 @@
 import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar'
+import HomeText from '../../components/HomeText/HomeText';
+import Projects from '../../components/Projects/Projects';
+import Info from '../../components/Info/Info'
 
 export default function App() {
   return (
     <main className="App">
       <NavBar />
+      <Routes>
+        <Route path='/home' element={<HomeText />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/info' element={<Info />} />
+        <Route path='/*' element={<Navigate to="/home" />} />
+      </Routes>
     </main>
   )
 }
