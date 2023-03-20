b5ad72e90917244a570ea3ff001a45cb12ebd598
szaccagni <sarahzaccagni@gmail.com>
2023-03-19 22:10:11 -0400

    adds some styling

--- src/components/HomeText/HomeText.jsx
+++ src/components/HomeText/HomeText.jsx
@@ -1,3 +1,3 @@
 export default function HomeText() {
-    return <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vel officiis, voluptates molestias illo excepturi quas voluptatibus eos perspiciatis est rem aperiam voluptas placeat natus optio nostrum, error tempora? Blanditiis.</div>
+    return <div>home</div>
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,14 @@
+import './NavBar.css'
+import { Link } from 'react-router-dom'
+
 export default function NavBar() {
     return (
-        <>
-            <h1>Sarah Zaccagni</h1>
-            <a>home</a>
-            <a>projects</a>
-            <a>info</a>
-            <a>contact</a>
-        </>
+        <div className='nav-bar'>
+            <div>SARAH ZACCAGNI</div>
+            <div class='nav-link'><Link to='/home'>HOME</Link></div>
+            <div class='nav-link'><Link to='/projects'>PROJECTS</Link></div>
+            <div class='nav-link'><Link to='/info'>INFO</Link></div>
+            <div class='nav-link'><Link to='/contact'>CONTACT</Link></div>
+        </div>
     )
 }
