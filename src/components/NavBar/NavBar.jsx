ff51136e346f0f129d7fc3056426128e1008f8c7
szaccagni <sarahzaccagni@gmail.com>
2023-03-20 00:58:51 -0400

    styles contact pg

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,30 @@
 import './NavBar.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function NavBar() {
+    const [activeLink, setActiveLink] = useState('')
+
+    const activeStyle = {
+        textDecoration: 'line-through',
+    }
+
     return (
         <div className='nav-bar'>
             <div className='nav-bar-title'><div>SARAH</div> &nbsp;<div>ZACCAGNI</div></div>
-            <div class='nav-link'><Link to='/home'>HOME</Link></div>
-            <div class='nav-link'><Link to='/projects'>PROJECTS</Link></div>
-            <div class='nav-link'><Link to='/info'>INFO</Link></div>
-            <div class='nav-link'><Link to='/contact'>CONTACT</Link></div>
+            <div class='nav-link'>
+                <Link to='/home' 
+                    style={ activeLink === 'home' ? activeStyle : {}}
+                    onClick={ () => setActiveLink('home')}>HOME</Link>
+            </div>
+            <div class='nav-link'>
+                <Link to='/projects' style={ activeLink === 'projects' ? activeStyle : {}}
+                onClick={ () => setActiveLink('projects')}>PROJECTS</Link>
+            </div>
+            <div class='nav-link'>
+                <Link to='/contact' style={ activeLink === 'contact' ? activeStyle : {}}
+                onClick={ () => setActiveLink('contact')}>CONTACT</Link>
+            </div>
         </div>
     )
 }
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,18 +1,21 @@
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 import NavBar from '../../components/NavBar/NavBar'
 import Home from '../../components/Home/Home';
 import Projects from '../../components/Projects/Projects';
-import Info from '../../components/Info/Info'
+import Contact from '../../components/Contact/Contact'
 
 export default function App() {
+  
+
   return (
     <main className="App">
       <NavBar />
       <Routes>
         <Route path='/home' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
-        <Route path='/info' element={<Info />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='/*' element={<Navigate to="/home" />} />
       </Routes>
     </main>
