1cc0aef8b4b39190132c05017a327ffec5197701
szaccagni <sarahzaccagni@gmail.com>
2023-03-20 03:18:08 -0400

    creates assets

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import './Home.css'
+import image1 from '../../assets/me3.jpg'
+import image2 from '../../assets/me4.JPG' 
 
 export default function Home() {
     return (
         <div className='home-container'>
             <div className='photo-booth-container'>
                 <div className='photo-booth'>
-                    <img src='me-3.jpg'></img> 
-                    <img src='me-4.JPG'></img>
+                    <img src={image1} alt='me'></img> 
+                    <img src={image2} alt='mef'></img>
                 </div>
             </div>
             <div className='home-text'><div>full</div> &nbsp;<div>stack</div> &nbsp;<div>developer</div></div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,16 +12,16 @@ export default function NavBar() {
     return (
         <div className='nav-bar'>
             <div className='nav-bar-title'><div>SARAH</div> &nbsp;<div>ZACCAGNI</div></div>
-            <div class='nav-link'>
+            <div className='nav-link'>
                 <Link to='/home' 
                     style={ activeLink === 'home' ? activeStyle : {}}
                     onClick={ () => setActiveLink('home')}>HOME</Link>
             </div>
-            <div class='nav-link'>
+            <div className='nav-link'>
                 <Link to='/projects' style={ activeLink === 'projects' ? activeStyle : {}}
                 onClick={ () => setActiveLink('projects')}>PROJECTS</Link>
             </div>
-            <div class='nav-link'>
+            <div className='nav-link'>
                 <Link to='/contact' style={ activeLink === 'contact' ? activeStyle : {}}
                 onClick={ () => setActiveLink('contact')}>CONTACT</Link>
             </div>
