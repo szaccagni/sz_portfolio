329c52a785d08b304d97d2f0920790d66d05014c
szaccagni <sarahzaccagni@gmail.com>
2023-03-21 21:20:56 -0400

    adds styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,35 @@
 import './Home.css'
 import image1 from '../../assets/me3.jpg'
 import image2 from '../../assets/me4.JPG' 
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Home({setActiveLink}) {
+    const [hoverScroll, setHoverScroll] = useState(false)
     
     useEffect( function() {
        setActiveLink('home')
     }, [])
 
+    function handleScroll(e) {
+        console.log(e.target.scrollTop)
+    }
+
     return (
         <div className='home-container'>
-            <div className='photo-booth-container'>
+            <div className= {hoverScroll ? 'photo-booth-container photo-booth-scroll' : 'photo-booth-container'} onMouseEnter={() => setHoverScroll(true)} onMouseLeave={() => setHoverScroll(false)} onScroll={handleScroll}>
                 <div className='photo-booth'>
                     <img src={image1} alt='me'></img> 
-                    <img src={image2} alt='mef'></img>
+                    {/* <img src={image2} alt='mef'></img> */}
+                    <div className='home-text'>
+                        <div>full</div> &nbsp;
+                        <div>stack</div> &nbsp;
+                        <div>developer</div>
+                    </div>
+                    <div className='home-text-small'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam corporis quam molestias! Assumenda, neque fugiat. Laborum architecto eligendi praesentium quaerat aliquid, consequatur quis, adipisci doloremque asperiores qui reprehenderit atque dicta.
+                    </div>
                 </div>
             </div>
-            <div className='home-text'><div>full</div> &nbsp;<div>stack</div> &nbsp;<div>developer</div></div>
         </div>
     )
 }
