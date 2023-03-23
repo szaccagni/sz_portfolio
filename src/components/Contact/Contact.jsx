a1121ac64ec4dcdee62b5206c2995b0220ec104f
szaccagni <sarahzaccagni@gmail.com>
2023-03-22 22:07:09 -0400

    fixes arrows

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,10 +10,10 @@ export default function Info({setActiveLink}) {
         <div className='contact-container'>
             <div className='contact-abs-container'>
                 <div className='contact-link'>
-                    <a href='https://github.com/szaccagni' target="_blank">GitHub <span style={{fontFamily: 'arimo-regular'}}>&#8599;</span></a>
+                    <a href='https://github.com/szaccagni' target="_blank">GitHub <span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
                 </div>
                 <div className='contact-link'>
-                    <a href='https://www.linkedin.com/in/szaccagni/' target="_blank">LinkedIn <span style={{fontFamily: 'arimo-regular'}}>&#8599;</span></a>
+                    <a href='https://www.linkedin.com/in/szaccagni/' target="_blank">LinkedIn <span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
                 </div>
                 <div className='contact-email' id='contact-email'>sarahzaccagni@gmail.com</div>
             </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,10 +10,10 @@ export default function Projects({setActiveLink}) {
         <div className='project-container'>
             <div className='project-abs-container'>
                 <div className='project-link'>
-                    <a href='https://trip-planner-sz-1212.herokuapp.com/' target="_blank"><div>trip</div>&nbsp;<div>planner</div><span style={{fontFamily: 'arimo-regular'}}>&#8599;</span></a>
+                    <a href='https://trip-planner-sz-1212.herokuapp.com/' target="_blank"><div>trip</div>&nbsp;<div>planner</div><span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
                 </div>
                 <div className='project-link'>
-                    <a href='https://szaccagni.github.io/battle_ship/' target="_blank">battleship <span style={{fontFamily: 'arimo-regular'}}>&#8599;</span></a>
+                    <a href='https://szaccagni.github.io/battle_ship/' target="_blank">battleship <span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
                 </div>
             </div>
         </div>
