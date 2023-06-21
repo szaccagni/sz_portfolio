ba916b6300566d47c0e9ab976ffe88760d1d2633
szaccagni <sarahzaccagni@gmail.com>
2023-06-21 17:11:50 -0400

    updates projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -13,10 +13,13 @@ export default function Projects({setActiveLink}) {
                     <a href='https://san-fran-curb-cuisine.vercel.app/' target="_blank"><div>curb</div>&nbsp;<div>cuisine</div><span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
                 </div>
                 <div className='project-link'>
-                    <a href='https://trip-planner-sz-1212.herokuapp.com/' target="_blank"><div>trip</div>&nbsp;<div>planner</div><span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
+                    <a href='https://carbon-footprint-calc.vercel.app/' target="_blank">carbon footprint quiz<span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
                 </div>
                 <div className='project-link'>
                     <a href='https://art-collector.herokuapp.com/' target="_blank">art collector <span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
+                </div>
+                <div className='project-link'>
+                    <a href='https://trip-planner-sz-1212.herokuapp.com/' target="_blank"><div>trip</div>&nbsp;<div>planner</div><span style={{fontFamily: 'Bootstrap-icons'}}>&#xF144;</span></a>
                 </div>
             </div>
         </div>
